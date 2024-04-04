0324b72035fe86289c89eb376d593a2b6607d1d5
luumanh <luumanhduc3105@gmail.com>
2024-04-04 22:48:33 +0700

    edit data binding.apply

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,26 +4,27 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
-    private  var binding: ActivityMainBinding? =null
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = setContentView(this, R.layout.activity_main)
         super.onCreate(savedInstanceState)
         initListeners()
     }
 
     private fun initListeners() {
-        binding?.bTn?.setOnClickListener {
-            val i = Intent(this@MainActivity, Ac_2::class.java)
-            i.putExtra("name", "John")
-            i.putExtra("age", 22)
-            i.putExtra("bienBool", true)
-            startActivity(i)
+            binding?.bTn?.setOnClickListener {
+                Intent(this, MainActivity2::class.java).apply {
+                    putExtra("name", "John")
+                    putExtra("age", 22)
+                    putExtra("bienBool", true)
+                    startActivity(this)
+                }
+            }
         }
     }
-}
--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -2,26 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.databinding.ActivityAc2Binding
 
-class Ac_2 : AppCompatActivity() {
-    private  var binding: ActivityAc2Binding?=null
+class MainActivity2 : AppCompatActivity() {
+    private var binding: ActivityAc2Binding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_ac2)
-        val i2 = intent
-        val name2 = i2.getStringExtra("name")
-        val age2 = i2.getIntExtra("age", 0)
-        binding.apply {
-            this?.n1?.text = name2
-            this?.a1?.text = age2.toString()
-        }
-        binding?.b1!!.setOnClickListener(){
-            finish()
+
+        val name = intent.getStringExtra("name")
+        val age = intent.getIntExtra("age", 0)
+        binding?.apply {
+            nameTextView.text = name
+            ageTextView.text = age.toString()
+            bTn2.setOnClickListener() {
+                finish()
+            }
         }
     }
 }
